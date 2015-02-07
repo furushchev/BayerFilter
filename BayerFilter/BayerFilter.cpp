bde07ec5fc5f07eca956e8f5a962b0e6fa9161c2
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2015-02-07 21:11:33 +0900

    add gitignore

---
+++ BayerFilter/BayerFilter.cpp
@@ -0,0 +1,9 @@
+//
+//  BayerFilter.cpp
+//  BayerFilter
+//
+//  Created by FurutaYuki on 2/6/15.
+//  Copyright (c) 2015 Furushchev. All rights reserved.
+//
+
+#include "BayerFilter.h"
---
+++ BayerFilter/BayerFilter.h
@@ -0,0 +1,14 @@
+//
+//  BayerFilter.h
+//  BayerFilter
+//
+//  Created by FurutaYuki on 2/6/15.
+//  Copyright (c) 2015 Furushchev. All rights reserved.
+//
+
+#ifndef __BayerFilter__BayerFilter__
+#define __BayerFilter__BayerFilter__
+
+#include <stdio.h>
+
+#endif /* defined(__BayerFilter__BayerFilter__) */
